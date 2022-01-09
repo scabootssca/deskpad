57f58c9ff8ea8a9653b6215c3e8910b9831357d8
scabootssca <scabootssca@gmail.com>
2022-01-08 18:20:35 -0600

    Updated to Gtk3 and Python3

--- deskpad.py
+++ deskpad.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -14,22 +14,28 @@
 # by: Jason Richards ~2011
 
 import os
-import gtk
-import glib
-import gobject
 
+import gi
 
-class DeskpadWindow(gtk.Window):
+gi.require_version('Gtk', '3.0')
+
+from gi.repository import Gtk
+from gi.repository import Gdk
+from gi.repository import GObject
+from gi.repository import GLib
+
+class DeskpadWindow(Gtk.Window):
 	def __init__(self):
-		gtk.Window.__init__(self)
+		Gtk.Window.__init__(self)
 
-		#self.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_TOOLBAR)
+		#self.startTypeHint = self.get_type_hint()
+		#self.set_type_hint(Gdk.WINDOW_TYPE_HINT_TOOLBAR)
 
 		self.opacityActive = 0.9
-		self.opacityHover = 0.5
-		self.opacityHidden = 0.02
+		self.opacityHover = 0.4
+		self.opacityHidden = 0.00
 
-		monitor = 0
+		monitorNum = 0
 		corner = 1
 		spacing = (20, 40)
 		size = (325, 1000)
@@ -40,53 +46,53 @@ class DeskpadWindow(gtk.Window):
 		self.resize(*size)
 
 		# Get all the screen size variables
-		offset = gtk.gdk.get_default_root_window().property_get(gtk.gdk.atom_intern('_NET_WORKAREA'))[2]
-		dimensions = list(gtk.gdk.screen_get_default().get_monitor_geometry(monitor))
+		display = Gdk.Display.get_default()
+		monitor = display.get_monitor(monitorNum)
+		dimensions = monitor.get_workarea()
+		#offset = Gdk.get_default_root_window().property_get(Gdk.atom_intern('_NET_WORKAREA'))[2]
 
 		# Calculate the position
 		if corner == 0:
-			(xPos, yPos) = (dimensions[0]+spacing[0], dimensions[1]+spacing[1])
+			(xPos, yPos) = (dimensions.x+spacing[0], dimensions.y+spacing[1])
 		elif corner == 1:
-			(xPos, yPos) = (dimensions[0]+dimensions[2]-spacing[0]-size[0], dimensions[1]+spacing[1])
+			(xPos, yPos) = (dimensions.x+dimensions.width-spacing[0]-size[0], dimensions.y+spacing[1])
 		elif corner == 2:
-			(xPos, yPos) = (dimensions[0]+spacing[0], dimensions[1]+dimensions[3]-size[1]-spacing[1])
+			(xPos, yPos) = (dimensions.x+spacing[0], dimensions.y+dimensions.height-size[1]-spacing[1])
 		elif corner == 3:
-			(xPos, yPos) = (dimensions[0]+dimensions[2]-spacing[0]-size[0], dimensions[1]+dimensions[3]-size[1]-spacing[1])
+			(xPos, yPos) = (dimensions.x+dimensions.width-spacing[0]-size[0], dimensions.y+dimensions.height-size[1]-spacing[1])
 
-		if xPos < offset[0]+spacing[0]:
-			xPos = offset[0]+spacing[0]
-		if yPos < offset[1]+spacing[1]:
-			yPos = offset[1]+spacing[1]
+		# if xPos < offset[0]+spacing[0]:
+			# xPos = offset[0]+spacing[0]
+		# if yPos < offset[1]+spacing[1]:
+			# yPos = offset[1]+spacing[1]
 
 		self.move(xPos, yPos)
 
-		self.text = gtk.TextView()
-		self.text.set_wrap_mode(gtk.WRAP_WORD_CHAR)
+		self.text = Gtk.TextView()
+		self.text.set_wrap_mode(Gtk.WrapMode.WORD_CHAR)
 		self.text.connect('button-press-event', self.event_button_press)
 		self.text.show()
 
 		# Load it
-		dataPath = '%s/deskpad.txt' % glib.get_user_data_dir()
+		dataPath = '%s/deskpad.txt' % GLib.get_user_data_dir()
 
 		if os.path.exists(dataPath):
-			with open(dataPath, 'rb') as fileHandle:
+			with open(dataPath, 'r') as fileHandle:
 				self.text.get_buffer().set_text(fileHandle.read())
 
-		scrolled = gtk.ScrolledWindow()
-		scrolled.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+		scrolled = Gtk.ScrolledWindow()
+		scrolled.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
 		scrolled.add(self.text)
 		scrolled.show()
 
-		self.pinnedLabel = gtk.Button('Unpin')
+		self.pinnedLabel = Gtk.Button(label='Unpin')
 		self.pinnedLabel.connect('clicked', self.event_button_pinned)
 
-		box = gtk.VBox()
-		box.pack_start(scrolled)
-		box.pack_end(self.pinnedLabel, False, True)
+		box = Gtk.VBox()
+		box.pack_start(scrolled, True, True, 0)
+		box.pack_end(self.pinnedLabel, False, True, 0)
 		box.show()
 		self.add(box)
-
-		self.set_opacity(self.opacityHidden)
 
 		self.connect('enter-notify-event', self.event_enter_notify, True)
 		self.connect('leave-notify-event', self.event_enter_notify, False)
@@ -104,14 +110,15 @@ class DeskpadWindow(gtk.Window):
 		self.set_can_focus(True)
 		self.stick()
 
-		gtk.Window.show(self)
+		Gtk.Window.show(self)
+		self.set_opacity(self.opacityHidden)
 
 	def event_button_pinned(self, button):
 		self.pinnedLabel.hide()
 		self.stuck = not self.stuck
 
 	def event_button_press(self, widget, event):
-		if event.type == gtk.gdk._2BUTTON_PRESS:
+		if event.type == Gdk.EventType._2BUTTON_PRESS:
 			if not self.stuck:
 				self.pinnedLabel.show()
 				self.set_opacity(1)
@@ -133,24 +140,24 @@ class DeskpadWindow(gtk.Window):
 		if focused:
 			self.set_opacity(self.opacityActive)
 		else:
-			# Save it
-			with open('%s/deskpad.txt' % glib.get_user_data_dir(), 'wb') as fileHandle:
-				fileHandle.write(self.text.get_buffer().get_text(*self.text.get_buffer().get_bounds()))
+			# # Save it
+			# with open('%s/deskpad.txt' % GLib.get_user_data_dir(), 'w') as fileHandle:
+				# fileHandle.write(self.text.get_buffer().get_text(*self.text.get_buffer().get_bounds()), True)
 			self.set_opacity(self.opacityHidden)
 
+	# We override this function to worry about self.stuck
 	def set_opacity(self, opacity):
 		if not self.stuck:
-			gtk.Window.set_opacity(self, opacity)
+			Gtk.Widget.set_opacity(self, opacity)
 
 	def destroy(self):
-		gtk.Window.destroy(self)
+		Gtk.Window.destroy(self)
 
 
 if __name__ == "__main__":
 	DeskpadWindow()
 
-	gobject.threads_init()
-	gobject.set_application_name('deskpad')
-	gobject.set_prgname('deskpad')
+	GLib.set_application_name('deskpad')
+	GLib.set_prgname('deskpad')
 
-	glib.MainLoop().run()
+	GLib.MainLoop().run()
